74e097529dc44b8d1f1fefb42f8ace5e2e441eeb
LavanyaaPrabhu <124457375+LavanyaaPrabhu@users.noreply.github.com>
2023-02-04 22:49:59 +0530

    Add files via upload
    Day1 assignments

---
+++ day1/Mobile.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mobileBrandName = "Samsung";
+		char mobileLogo = 'S';
+		short noOfMobilePiece = 10;
+		int modelNumber = 2345;
+		long mobilemeiNumber = 8939559652L;
+		float mobilePrice = 25000.45F;
+		boolean isDamaged = false;
+		
+		System.out.println(mobileBrandName);
+		System.out.println(mobileLogo);
+		System.out.println(noOfMobilePiece);
+		System.out.println(modelNumber);
+		System.out.println(mobilemeiNumber);
+		System.out.println(mobilePrice);
+		System.out.println(isDamaged);
+		
+		
+		
+		
+
+	}
+
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int noOfWheels =4;
+		short noOfMirrors = 2;
+		long chassisNumber = 545609712345679l;
+		boolean isPunctured = false;
+		String carName ="Verna";
+		double runningKM= 2500.456;
+		
+		System.out.println(noOfWheels);
+		System.out.println(noOfMirrors);
+		System.out.println(chassisNumber);
+		System.out.println(isPunctured);
+		System.out.println(carName);
+		System.out.println(runningKM);
+		
+		
+		
+
+	}
+
+}
